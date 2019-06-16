ca0648327d83ebbd68176af144286d96c3e7a0f1
Jean <BlasterJhan@gmail.com>
2019-06-16 14:32:14 +0300

    Тестовое задание, сделал, что смог.

---
+++ Assets/Scripts/New_Collor.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class New_Collor : MonoBehaviour
+{
+    Renderer rend;
+
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.E))
+        {
+            rend.material.color = Color.grey;
+        }
+
+        if (Input.GetKey(KeyCode.R))
+        {
+            rend.material.color = Color.red;
+        }
+    }
+}
+
